a375f4adb551fdd7aa4ed79772c0103c66413a3f
M Clark <mclark@nvidia.com>
2013-09-06 11:29:02 -0700

    Added missing GaugeFieldParam settings.

--- milc_interface/milc_update_interface.cpp
+++ milc_interface/milc_update_interface.cpp
@@ -15,14 +15,17 @@ void  qudaUpdateU(int prec, int dim[4], double eps, void* momentum, void* link)
 
   QudaGaugeParam gaugeParam = newQudaGaugeParam();
 
-  gaugeParam.cpu_prec = gaugeParam.cuda_prec = (prec==1) ? QUDA_SINGLE_PRECISION : QUDA_DOUBLE_PRECISION;
-  gaugeParam.reconstruct = QUDA_RECONSTRUCT_NO;
+  gaugeParam.cpu_prec = gaugeParam.cuda_prec = gaugeParam.cuda_prec_sloppy = 
+    (prec==1) ? QUDA_SINGLE_PRECISION : QUDA_DOUBLE_PRECISION;
+  gaugeParam.reconstruct = gaugeParam.reconstruct_sloppy = QUDA_RECONSTRUCT_NO;
   gaugeParam.gauge_fix = QUDA_GAUGE_FIXED_NO;
   gaugeParam.anisotropy = 1.0;
   gaugeParam.tadpole_coeff = 1.0;
   gaugeParam.ga_pad = 0;
   gaugeParam.scale = 1.;
   gaugeParam.type = QUDA_GENERAL_LINKS;
+  gaugeParam.gauge_order = QUDA_MILC_GAUGE_ORDER;
+  gaugeParam.t_boundary = QUDA_PERIODIC_T;
 
   for(int dir=0; dir<4; ++dir) gaugeParam.X[dir] = dim[dir];
 
